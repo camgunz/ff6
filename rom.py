a9c0d9194c6bfecc20a5a161b4d48b91701c4aca
Charlie Gunyon <charles.gunyon@gmail.com>
2017-01-26 17:42:47 -0500

    Fix bugs in ROM class Update patch_list Add a couple scripts to set whether or not a patch needs a header

--- rom.py
+++ rom.py
@@ -31,6 +31,19 @@ class ROM:
             loc = struct.unpack_from('3B', patch_data, offset=offset)
             offset += 3
             if loc == self.IPS_EOF_BYTES:
+                if len(patch_data) - offset == 3:
+                    truncate = struct.unpack_from(
+                        '3B',
+                        patch_data,
+                        offset=offset
+                    )
+                    offset += 3
+                    truncate = (
+                        (truncate[0] << 16) +
+                        (truncate[1] << 8) +
+                        (truncate)
+                    )
+                    self.rom_data = self.rom_data[:truncate]
                 break
             loc = ((loc[0] << 16) + (loc[1] << 8)  + (loc[2]))
             patch_size = struct.unpack_from('!H', patch_data, offset=offset)
@@ -40,10 +53,10 @@ class ROM:
                 run_size = struct.unpack_from('!H', patch_data, offset=offset)
                 offset += 2
                 run_size = run_size[0]
-                fill_char = struct.unpack_from('c', patch_data, offset=offset)
+                fill_byte = struct.unpack_from('c', patch_data, offset=offset)
                 offset += 1
-                fill_char = fill_char[0]
-                self.rom_data[loc:loc+run_size] = fill_char * run_size
+                fill_byte = fill_byte[0]
+                self.rom_data[loc:loc+run_size] = fill_byte * run_size
             else:
                 replacement_data = struct.unpack_from(
                     '!{}s'.format(patch_size),
@@ -66,7 +79,7 @@ class ROM:
 
     def ensure_has_header(self):
         if not self.has_header:
-            self.rom_data.insert(0, 0x00 * 512)
+            self.rom_data = bytearray((0,) * 512) + self.rom_data
             self.header_size = 512
             self.has_header = True
 
---
+++ set_needs_no_header.py
@@ -0,0 +1,25 @@
+import os
+import lib
+import sys
+import shutil
+
+def main():
+    if not len(sys.argv) == 2:
+        sys.stderr.write('Must specify name of patch\n\n')
+        os.exit(-2)
+    config = lib.get_config()
+    old_patch_name = sys.argv[1]
+    new_patch_name = old_patch_name.replace('-h10.ips', '-nh10.ips')
+    old_patch_path = os.path.join(config['patch_dir'], old_patch_name)
+    new_patch_path = os.path.join(config['patch_dir'], new_patch_name)
+    shutil.move(old_patch_path, new_patch_path)
+    with open(config['patch_list'], 'r') as fobj:
+        old_patch_list_data = fobj.read()
+    new_patch_list_data = old_patch_list_data.replace(
+        old_patch_name,
+        new_patch_name
+    )
+    with open(config['patch_list'], 'w') as fobj:
+        fobj.write(new_patch_list_data)
+
+main()
