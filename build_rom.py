fb260390e03da2b751ec1984caef993419826571
Charlie Gunyon <charles.gunyon@gmail.com>
2017-01-27 03:30:01 -0500

    Mostly figure out patches; there's a bug where L runs in battles, but the only worry there is there are other lingering effects

--- build_rom.py
+++ build_rom.py
@@ -1,7 +1,26 @@
+import os
 import lib
+import rom
 
 def main():
-    pass
+    config = lib.get_config()
+    patch_list = lib.get_patch_list()
+    ff3 = rom.ROM.from_file(config['base_rom'])
+    n = 1
+    for patch_file_name, requires_header, apply in lib.get_patch_list():
+        if not apply:
+            continue
+        if requires_header:
+            ff3.ensure_has_header()
+        else:
+            ff3.ensure_has_no_header()
+        patch_file_path = os.path.join(config['patch_dir'], patch_file_name)
+        print('Applying patch {} ({}/{})'.format(
+            patch_file_name, n, len(patch_list),
+        ))
+        ff3.apply_ips_patch_file(patch_file_path)
+        n += 1
+    ff3.save_to_file(config['project_rom'])
 
 main()
 
--- lib.py
+++ lib.py
@@ -1,7 +1,26 @@
+import csv
+
 from configparser import ConfigParser
 
 __PROJECT_CONFIG_FILE_NAME = 'project.ini'
 __CONFIG = None
+__PATCH_LIST = None
+
+def get_patch_list():
+    global __PATCH_LIST
+    if __PATCH_LIST is None:
+        config = get_config()
+        patch_list_file_name = config['patch_list']
+        patch_list = []
+        with open(patch_list_file_name, 'r') as csv_fobj:
+            for row in csv.DictReader(csv_fobj):
+                patch_list.append((
+                    row['file_name'],
+                    row['header'] == 'y',
+                    row['apply'] == 'y'
+                ))
+        __PATCH_LIST = patch_list
+    return __PATCH_LIST
 
 def get_config():
     global __CONFIG
