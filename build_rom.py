5c7be7cfc485a60703c0e20a0a48e1843f1171ee
Charlie Gunyon <charles.gunyon@gmail.com>
2017-01-27 14:45:14 -0500

    Use an advanced save to find more patch issues (turns out L running was a misconfigured SNES9x, btw)

--- build_rom.py
+++ build_rom.py
@@ -2,12 +2,19 @@ import os
 import lib
 import rom
 
+STOP_AFTER_MIN_PATCH = False
+STOP_BEFORE_FF3_PATCHES = False
+STOP_AFTER_PATCH_COUNT = None
+
 def main():
     config = lib.get_config()
     patch_list = lib.get_patch_list()
     ff3 = rom.ROM.from_file(config['base_rom'])
     n = 1
     for patch_file_name, requires_header, apply in lib.get_patch_list():
+        if STOP_BEFORE_FF3_PATCHES:
+            if patch_file_name == 'FF3-AnonymousAttack-N0.ips':
+                break
         if not apply:
             continue
         if requires_header:
@@ -20,6 +27,10 @@ def main():
         ))
         ff3.apply_ips_patch_file(patch_file_path)
         n += 1
+        if STOP_AFTER_MIN_PATCH and patch_file_name == 'RefBeam.ips':
+            break
+        if STOP_AFTER_PATCH_COUNT and n == STOP_AFTER_PATCH_COUNT:
+            break
     ff3.save_to_file(config['project_rom'])
 
 main()
