e5e980f035ec8ac3036f32af71433f109d9188d4
Charlie Gunyon <charles.gunyon@gmail.com>
2017-12-18 01:20:21 -0500

    Remove some old code

--- ff6/util.py
+++
@@ -1,5 +0,0 @@
-def check_only_one(attributes, names):
-    if len(list(filter(None, [attributes[name] for name in names]))) > 1:
-        msg = 'Only one of %s or %s may be given'
-        raise ValueError(msg % (', '.join(names[:1]), names[-1]))
-
